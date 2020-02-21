66b87006b729a9bba799690c32ccca1d2d910150
salubrious-commit-8954 <wangyy395@gmail.com>
2020-02-21 20:18:05 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -5,7 +5,7 @@ end
 
 def my_hash
   # return a valid hash with any key/value pair of your choice
-  myHash = {newHash: whatever}
+  myHash = {newHash: whatever, nextelement: dontknow}
 end
 
 def pioneer
